9e7455e8e2f484a406df06f845c9b859cb29d08d
Jimmy Lee <jimmylee88@users.noreply.github.com>
2025-07-20 20:02:07 +0100

    Added folder with working files

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,21 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'superhero-location-answer'
+router.post('/superhero-location-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'superhero-location'
+  // references the radio class of superheroLocation not the URL of the page
+  var isUKLocation = req.session.data['superheroLocation']
+
+  // Check whether the variable matches a condition
+  if (isUKLocation == "abroad"){
+    // Send user to next page
+    res.redirect('/ineligible')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/superhero-power')
+  }
+
+})
